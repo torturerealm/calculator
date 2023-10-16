f4665393d7343638e7a1348f1c35c3788238e004
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-10-16 18:24:27 +0800

    Create operate function
    This function takes the first number, second number, and operator as parameters.
    It calls math functions depending on the operator.
    Ex: if operator is "+", it calls upon add function.

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+let numberOne = 3;
+let numberTwo = 2;
+let operator = "+";
+let equals = 0;
+
 function add(a, b) {
     return a + b;
-};
+}
 
 function subtract(a, b) {
     return a - b;
-};
+}
 
 function multiply(a, b) {
     return a * b;
-};
+}
 
 function divide(a, b) {
     return a / b;
-};
+}
 
-console.log(add(1, 2));
+function operate(a, b, operator) {
+    if (operator === "+") {
+        equals = add(a, b);
+    } else if (operator === "-") {
+        subtract(a, b);
+    } else if (operator === "*") {
+        multiply(a, b);
+    } else if (operator === "/") {
+        divide(a, b);
+    };
+}
+
+operate(numberOne, numberTwo, operator);
+console.log(equals);
